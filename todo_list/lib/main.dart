a6572ac6e28084dbb23e797a536250561e6c6511
Kishor  Kumar Ghadei <100041716+kishor7008@users.noreply.github.com>
2023-11-07 22:31:09 +0530

    Update main.dart

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:part_1/todo_model.dart';
-import 'package:part_1/todo_screen.dart';
-import 'package:provider/provider.dart';
+// import 'package:part_1/todo_model.dart';
+// import 'package:part_1/todo_screen.dart';
+// import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
